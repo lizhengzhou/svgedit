b38341d79a524edf20d03d102332cb6bd0d0f613
lizz <374494848@qq.com>
2018-08-07 09:51:56 +0800

    本地 保存

--- editor/extensions/ext-locale/storage/zh_CN.js
+++ editor/extensions/ext-locale/storage/zh_CN.js
@@ -1,20 +1,13 @@
-var svgEditorExtensionLocale_storage_zh_CN = (function () {
-  'use strict';
-
-  var zh_CN = {
-    message: '默认情况下，如果浏览器支持，编辑器可以在您的计算机上存储编辑器 ' 
-    + '首选项和绘制内容，因此您无需在每次加载时都配置这些首选项和重新 ' 
-    + '绘制内容。 如果出于隐私原因，您不希望将此信息存储在您的计算机 ' 
-    + '上，则可以更改以下默认选项。 ',
-    storagePrefsAndContent: '将编辑器首选项和绘制内容保存在本地',
-    storagePrefsOnly: '只保存首选项',
-    storagePrefs: '只保存首选项',
-    storageNoPrefsOrContent: '不要将编辑器首选项和绘制内容保存在本地',
-    storageNoPrefs: '编辑器首选项和绘制内容保存在本地',
-    rememberLabel: '记住我的选择 ?',
-    rememberTooltip: '记住选择避免每次都问一遍。'
-  };
-
-  return zh_CN;
-
-}());
+export default {
+  message: '默认情况下，如果浏览器支持，编辑器可以在您的计算机上存储编辑器 ' +
+    '首选项和绘制内容，因此您无需在每次加载时都配置这些首选项和重新 ' +
+    '绘制内容。 如果出于隐私原因，您不希望将此信息存储在您的计算机 ' +
+    '上，则可以更改以下默认选项。 ',
+  storagePrefsAndContent: '将编辑器首选项和绘制内容保存在本地',
+  storagePrefsOnly: '只保存首选项',
+  storagePrefs: '只保存首选项',
+  storageNoPrefsOrContent: '不要将编辑器首选项和绘制内容保存在本地',
+  storageNoPrefs: '编辑器首选项和绘制内容保存在本地',
+  rememberLabel: '记住我的选择 ?',
+  rememberTooltip: '记住选择避免每次都问一遍。'
+};
