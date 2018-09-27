92e127c36efbe73f40088d3894be58ecb5e40cd5
lizz <374494848@qq.com>
2018-09-27 14:56:20 +0800

    注释

--- editor/extensions/ext-wcsdrawroad.js
+++ editor/extensions/ext-wcsdrawroad.js
@@ -20,10 +20,18 @@ export default {
     const svgCanvas = svgEditor.canvas;
     const addElem = S.addSvgElementFromJson;
     const getNextId = S.getNextId;
+    const svgUtils = svgCanvas.getPrivateMethods();
     const seNs = svgCanvas.getEditorNS(true);
 
     let focusPoint, IsDrawing, startElem, endElem, currentRoute, IsControl, controlElem;
     const currentStrokeWidth = 4, pointRadius = 2, controlRadius = 4;
+    let startElemCmd;
+
+    const {
+      InsertElementCommand,
+      ChangeElementCommand,
+      BatchCommand
+    } = svgUtils;
 
     return {
       name: 'drawroad',
@@ -42,6 +50,9 @@ export default {
         if (svgCanvas.getMode() === 'drawroad') {
           const zoom = svgCanvas.getZoom();
           if (!IsDrawing) {
+            /**
+             * 如果选择了起始点，则使用起始点画路线；如果没有选择起始点，则新画一个起始点
+             */
             let x, y;
             IsDrawing = true;
             if (focusPoint) {
@@ -80,6 +91,9 @@ export default {
               }
             });
           } else if (IsControl) {
+            /**
+             * 画控制点
+             */
             const x = opts.start_x,
               y = opts.start_y;
 
@@ -112,6 +126,9 @@ export default {
       mouseMove: function mouseMove (opts) {
         if (svgCanvas.getMode() === 'drawroad') {
           if (!IsDrawing) {
+            /**
+             * 突出显示选中的点
+             */
             const mouseTarget = opts.event.target;
             if (mouseTarget && mouseTarget !== focusPoint && mouseTarget.tagName === 'circle' && mouseTarget.getAttribute('class') === 'point') {
               focusPoint = mouseTarget;
@@ -122,6 +139,9 @@ export default {
               focusPoint = null;
             }
           } else {
+            /**
+             * 移动路线终点或者控制点
+             */
             const zoom = svgCanvas.getZoom();
             const x = opts.mouse_x / zoom,
               y = opts.mouse_y / zoom;
@@ -144,6 +164,9 @@ export default {
       mouseUp (opts) {
         if (svgCanvas.getMode() === 'drawroad') {
           if (IsDrawing && !IsControl) {
+            /**
+             * 画终点并结束
+             */
             const curve = currentRoute.pathSegList.getItem(1);
 
             const zoom = svgCanvas.getZoom();
@@ -164,12 +187,21 @@ export default {
             endElem.setAttributeNS(seNs, 'se:routes', currentRoute.id);
 
             let routeAttr = startElem.getAttributeNS(seNs, 'routes');
+            const orgRouteAttr = routeAttr;
             if (!routeAttr) {
               routeAttr = currentRoute.id;
             } else {
               routeAttr += ' ' + currentRoute.id;
             }
             startElem.setAttributeNS(seNs, 'se:routes', routeAttr);
+
+            if (!orgRouteAttr) {
+              startElemCmd = new InsertElementCommand(startElem);
+            } else {
+              startElemCmd = new ChangeElementCommand(startElem, {
+                'se:routes': orgRouteAttr
+              });
+            }
 
             currentRoute.setAttributeNS(seNs, 'se:points', startElem.id + ' ' + endElem.id);
 
@@ -182,9 +214,18 @@ export default {
               focusPoint = null;
             }
 
+            /**
+             * 如果按了空格键，则继续点击画控制点
+             */
             if (svgCanvas.spaceKey) {
               IsControl = true;
             } else {
+              const batchCmd = new BatchCommand();
+              batchCmd.addSubCommand(new InsertElementCommand(endElem));
+              batchCmd.addSubCommand(new InsertElementCommand(currentRoute));
+              batchCmd.addSubCommand(startElemCmd);
+              S.addCommandToHistory(batchCmd);
+
               IsDrawing = false;
               svgEditor.clickSelect();
             }
@@ -193,6 +234,16 @@ export default {
               keep: true
             };
           } else if (IsDrawing && IsControl) {
+            /**
+             * 结束弧线绘制
+             */
+            const batchCmd = new BatchCommand();
+            batchCmd.addSubCommand(new InsertElementCommand(controlElem));
+            batchCmd.addSubCommand(new InsertElementCommand(endElem));
+            batchCmd.addSubCommand(new InsertElementCommand(currentRoute));
+            batchCmd.addSubCommand(startElemCmd);
+            S.addCommandToHistory(batchCmd);
+
             IsControl = false;
             IsDrawing = false;
             svgEditor.clickSelect();
